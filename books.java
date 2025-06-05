1913ac16ba7a15564ca2274eebdfab114bc00def
Rishi23-02 <143711377+Rishi23-02@users.noreply.github.com>
2025-06-05 09:49:14 +0000

    Pending changes exported from your codespace

---
+++ books.java
@@ -0,0 +1,81 @@
+
+
+/**
+ * Class books
+ */
+public class books {
+
+  //
+  // Fields
+  //
+
+  public void title;
+  public void author_name;
+  
+  //
+  // Constructors
+  //
+  public books () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of title
+   * @param newVar the new value of title
+   */
+  public void setTitle (void newVar) {
+    title = newVar;
+  }
+
+  /**
+   * Get the value of title
+   * @return the value of title
+   */
+  public void getTitle () {
+    return title;
+  }
+
+  /**
+   * Set the value of author_name
+   * @param newVar the new value of author_name
+   */
+  public void setAuthor_name (void newVar) {
+    author_name = newVar;
+  }
+
+  /**
+   * Get the value of author_name
+   * @return the value of author_name
+   */
+  public void getAuthor_name () {
+    return author_name;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   * @return       char
+   */
+  public char show_book()
+  {
+  }
+
+
+  /**
+   * @return       int
+   */
+  public int show_price()
+  {
+  }
+
+
+}
